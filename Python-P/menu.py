512117b6c28c2db5ad11457f554afd48f90aa236
Umar FArooq <umar5555f@gmail.com>
2025-07-23 03:39:35 +0500

    New codes

---
+++ Python-P/menu.py
@@ -0,0 +1,12 @@
+# build Menu for a cafe
+
+title = "menu".upper()
+print(title.center(20, "="))  # Center the title with asterisks
+print("1. Coffee".ljust(16, ".") + "$2".rjust(4))  # Print coffee option
+print("2. Muffin".ljust(16, ".") + "$2".rjust(4))  # Print muffin option
+print("3. Cheesecake".ljust(16, ".") + "$2".rjust(4))  # Print muffin option
+
+
+print(" ")
+
+# string index values
